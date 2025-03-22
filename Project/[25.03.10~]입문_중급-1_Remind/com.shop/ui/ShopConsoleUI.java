9b722a4dc2b2f05bfc1f212aeb598c028a6065aa
seong_hyun_notebook <getplay0131@gmail.com>
2025-03-23 00:10:59 +0900

    25.03.22 1st Check

--- Project/[25.03.10~]입문_중급-1_Remind/com.shop/ui/ShopConsoleUI.java
+++ Project/[25.03.10~]입문_중급-1_Remind/com.shop/ui/ShopConsoleUI.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import model.cart.Cart;
+import model.product.Category_Enum;
 import model.product.Product;
 import service.*;
 
@@ -29,7 +31,7 @@ public class ShopConsoleUI {
 
         boolean isEndRoop = false;
 //        * 3. 메인 메뉴 표시 및 처리 메서드 구현
-        while (isEndRoop) {
+        while (!isEndRoop) {
             displayMainMenu();
             String choice = reader.readLine();
 
@@ -44,38 +46,112 @@ public class ShopConsoleUI {
         System.out.println("2. 장바구니 관리");
         System.out.println("3. 상품 관리");
         System.out.println("4. 주문 내역 관리");
-        System.out.println("-- 번호를 입력해주세요! ex)1번 or 1 --");
+        System.out.println("-- 원하시는 기능의 번호를 입력해주세요! ex)1번 or 1 --");
     }
 
     public static Object returnService(String choice) {
         //         * 2. 각 서비스 클래스의 인스턴스 참조
 //        서비스 인스턴스 호출
         UserService userService = UserService.getUserService();
-        CartService cartService = CartService.getCartService();
-        ProductService productService = ProductService.getProductService();
         OrderService orderService = OrderService.getOrderService();
         if (choice.contains("1")) {
 //        사용자 정보 관리
             return userService;
         } else if (choice.contains("2")) {
 //      장바구니 관리
-            return cartService;
         } else if (choice.contains("3")) {
 //      상품 관리
-            return productService;
+
         } else if (choice.contains("4")) {
 //      주문 관리
-            return  orderService;
+            return orderService;
         } else {
             System.out.println("올바른 값을 입력해주세요!");
         }
         return null;
     }
 
-//    * 4. 상품 목록 표시 메서드 구현
-    public void displayProductList(ArrayList productList){
-        if (!productList.isEmpty()) {
+    //    * 4. 상품 목록 표시 메서드 구현
+    public static void productListDisplay() {
+        ProductService productService = ProductService.getProductService();
 
+        ArrayList<Product> productList = productService.getProductList();
+        if (!productList.isEmpty()) {
+            System.out.println(" == 상품 목록 == ");
+            for (Product product : productService.getProductList()) {
+                for (int i = 0; i < productList.size(); i++) {
+                    System.out.println((i + 1) + "번 상품 : " + product.getName());
+                }
+            }
+            System.out.println(" == 상품 목록 출력 종료 == ");
+        } else {
+            System.out.println("등록된 상품이 없습니다.");
+        }
+    }
+
+
+    //    * 5. 상품 상세 정보 표시 메서드 구현
+    public static void productInfoDisplay() {
+        ProductService productService = ProductService.getProductService();
+
+        ArrayList<Product> productList = productService.getProductList();
+        if (!productList.isEmpty()) {
+            System.out.println(" == 상품 상세 정보 == ");
+            for (Product product : productService.getProductList()) {
+                System.out.println(product.toString());
+
+            }
+            System.out.println(" == 상품 상세 정보 출력 종료 == ");
+        } else {
+            System.out.println("등록된 상품이 없습니다.");
+        }
+    }
+
+    //* 6. 장바구니 표시 및 관리 메서드 구현
+    public static void displayCart() {
+        CartService cartService = CartService.getCartService();
+        ArrayList<Cart> cartList = cartService.getCartArrayList();
+        if (!cartList.isEmpty()) {
+            System.out.println("== 장바구니입니다! ==");
+            for (Cart cart : cartList) {
+                cart.displayCart();
+            }
+            System.out.println("== 장바구니 표시 종료 ==");
+        }
+    }
+
+//    if vs try - catch
+    public static void cartManagers(BufferedReader reader) throws Exception {
+        CartService cartService = CartService.getCartService();
+        ArrayList<Cart> cartList = cartService.getCartArrayList();
+        boolean isCheckInputValueError = false;
+        if (!cartList.isEmpty()) {
+            System.out.println("== 장바구니 관리 시스템입니다! ==");
+            System.out.println("무슨 작업을 진행할까요?");
+            System.out.println("상품을 추가하려면 '추가', 삭제하려면 '삭제'라고 입력해주세요!");
+            String choiceWork = reader.readLine();
+
+            if (choiceWork.contains("추가")) {
+                System.out.println("추가할 상품의 아이디를 입력해주세요");
+                String newProductId = reader.readLine();
+                if (newProductId.isEmpty()) {
+
+                }
+                System.out.println("추가할 상품의 상품명를 입력해주세요");
+                String newProductName = reader.readLine();
+                System.out.println("추가할 상품의 상품 가격을 입력해주세요");
+                int newPrice = Integer.parseInt(reader.readLine());
+                System.out.println("추가할 상품의 상품 재고를 입력해주세요");
+                int newStock = Integer.parseInt(reader.readLine());
+                System.out.println("추가할 상품의 카테고리를 입력해주세요");
+                String newCategory = reader.readLine();
+                ProductService.getProductService().addProduct(new Product());
+                for (Cart cart : cartList) {
+                    cart.addItem();
+                }
+
+            }
+            System.out.println("== 장바구니 표시 종료 ==");
         }
     }
 }
--- [노트북]코딩 테스트/프로그래머스/Exam_250314/문자열계산하기.java
+++ [노트북]코딩 테스트/프로그래머스/Exam_250314/문자열계산하기.java
@@ -1,9 +1,20 @@
 package Exam_250314;
-//문제 설명
 //my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
 public class 문자열계산하기 {
     public int solution(String my_string) {
         int answer = 0;
+        if (my_string.contains("+")) {
+            String[] split = my_string.replace(" + ", "+").split("\\+");
+            for (String string : split) {
+                answer += Integer.parseInt(string);
+            }
+        } else if (my_string.contains("-")) {
+            String[] split = my_string.replace(" - ", "-").split("\\-");
+            for (String string : split) {
+                answer -= Integer.parseInt(string);
+            }
+        }
+        System.out.println(answer);
         return answer;
     }
 
@@ -11,9 +22,14 @@ public class 문자열계산하기 {
     public static void main(String[] args) {
         String my_string = "3 - 4";
         int answer = 0;
-        int startIdx = 0;
-        int total = 0;
-        int temp1 = 0;
+        int count = 0;
+        if (my_string.contains("+")) {
+            my_string.replace(" + ", "+");
+        } else if (my_string.contains("-")) {
+            my_string.replace(" - ", "-");
+        }
+
+
         System.out.println(answer);
     }
 }
